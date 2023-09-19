a644115fde27f02d423966a51bc40151341691c3
guodegang <1481885191@qq.com>
2023-09-19 22:48:18 +0800

    docs: create project syt

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <HospitalTop />
+    <div class="content">
+      <router-view></router-view>
+    </div>
+    <HospitalBottom />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+
+  .content {
+    margin-top: 70px;
+    width: 1200px;
+    height: 700px;
+  }
+}
+</style>
